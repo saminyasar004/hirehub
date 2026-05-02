deb07b6e166ebf38034267f73320c52c03e01677
Samin Yasar <yasarsamin57@gmail.com>
2026-05-03 01:00:55 +0600

    feat: implement user-service core infrastructure with Prisma, Hono, and Redis event subscription

---
+++ packages/types/src/index.ts
@@ -0,0 +1,14 @@
+export type UserRegisteredEventPayload = {
+	userId: number;
+	role: string;
+	firstName: string;
+	lastName: string;
+	bio: string;
+	address: string;
+	resume: string;
+	avatar: string;
+	phone: string;
+	github: string;
+	linkedin: string;
+	website: string;
+};
